9a612e5ffb3557f3dcf23fe4633ae8ede669cd56
prasad <prasad@gmail.com>
2024-12-19 15:31:07 +0530

    java scripts

---
+++ maven.java
@@ -0,0 +1,11 @@
+// Using a for loop
+for (let i = 0; i < 5; i++) {
+  console.log(i);  // Output: 0, 1, 2, 3, 4
+}
+
+// Using a while loop
+let j = 0;
+while (j < 5) {
+  console.log(j);  // Output: 0, 1, 2, 3, 4
+  j++;
+}
